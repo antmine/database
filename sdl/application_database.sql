cfb3061c37c732b8a2eb9217e6023318ee19b6ae
Aljosha Armitano <aljosha@antmine.io>
2017-03-28 15:34:56 -0400

    recreate APPLICATION.MINER_EVENT table with correct events

--- sdl/application_database.sql
+++ sdl/application_database.sql
@@ -10,8 +10,6 @@ use APPLICATION;
 drop table if exists MINER;
 drop table if exists MINER_EVENT;
 drop table if exists MINER_WEBSITE;
-drop table if exists CRYPTO_MINER;
-drop table if exists CRYPTO;
 
 /* Table MINER  */
 create table MINER (
@@ -32,31 +30,11 @@ create table MINER_WEBSITE (
 /* Table MINER_EVENT */
 create table MINER_EVENT (
     ID_MINER int not null,
-    TIME_EVENT timestamp not null,
-    URL varchar(255) null,
-    COUNTRY varchar(255) null,
-    BROWSER varchar(255) null,
-    ENVIRONEMENT varchar(255) null,
-    TYPE_EVENT int not null,
+    TIME_EVENT timestamp not null default CURRENT_TIMESTAMP,
+    URL varchar(255) not null,
+	TAB_ACTIVE boolean null,
+    ON_BATTERY boolean null,
+    DISCONNECTED boolean null,
     constraint PK_MINER_EVENT primary key (ID_MINER, TIME_EVENT),
     constraint FK_MINER_EVENT_ID_MINER foreign key (ID_MINER) references MINER(ID_MINER)
-);
-
-
-/* Table CRYPTO */
-create table CRYPTO (
-    ID_CRYPTO int not null,
-    CRYPTO_TYPE int not null,
-    CRYPTO_VALUE varchar(255),
-    constraint PK_CRYPTO primary key (ID_CRYPTO) 
-);
-
-/* Table CRYPTO_MINER */
-create table CRYPTO_MINER (
-    ID_CRYPTO int not null,
-    ID_MINER int not null,
-    TIME_CREATION timestamp not null,
-    constraint PK_CRYPTO_MINER primary key (ID_CRYPTO, ID_MINER),
-    constraint FK_CRYPTO_MINER_ID_CRYPTO foreign key (ID_CRYPTO) references CRYPTO(ID_CRYPTO),
-    constraint FK_CRYPTO_MINER_ID_MINER foreign key (ID_MINER) references MINER(ID_MINER)
 );
